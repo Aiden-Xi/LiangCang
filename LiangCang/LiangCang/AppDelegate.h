6e8a6901f4c04a82f394f37a77ffdc84f7023bf9
Mr.Xi <xxy380491563@163.com>
2016-01-19 22:04:20 +0800

    first

---
+++ LiangCang/LiangCang/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LiangCang
+//
+//  Created by Mr.Xi on 16/1/19.
+//  Copyright © 2016年 Mr.Xi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
