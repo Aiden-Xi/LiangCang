e7361a4f58cf19c1b44fe335339cbc5bf865e81b
xixiaoyu <xixiaoyu@yundianjia.com>
2016-01-29 20:01:57 +0800

    添加跳转

--- LiangCang/Shop/Controllers/CartViewController.h
+++ LiangCang/Shop/Controllers/CartViewController.h
@@ -1,13 +1,13 @@
 //
-//  CatViewController.m
+//  CatViewController.h
 //  LiangCang
 //
 //  Created by Mr.Xi on 16/1/28.
 //  Copyright © 2016年 Mr.Xi. All rights reserved.
 //
 
-#import "CatViewController.h"
+#import "BaseViewController.h"
 
-@implementation CatViewController
+@interface CartViewController : BaseViewController
 
 @end
--- LiangCang/Shop/Controllers/CartViewController.m
+++ LiangCang/Shop/Controllers/CartViewController.m
@@ -1,13 +1,13 @@
 //
-//  CatViewController.h
+//  CatViewController.m
 //  LiangCang
 //
 //  Created by Mr.Xi on 16/1/28.
 //  Copyright © 2016年 Mr.Xi. All rights reserved.
 //
 
-#import "BaseViewController.h"
+#import "CartViewController.h"
 
-@interface CatViewController : BaseViewController
+@implementation CartViewController
 
 @end
